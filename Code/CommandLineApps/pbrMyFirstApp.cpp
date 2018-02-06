b0f7cc500e790d010b53a88cb09bbd0d761ca356
Matt Clarkson <m.clarkson@ucl.ac.uk>
2018-02-06 09:41:13 +0000

    Remove redundant code in first command line app.

--- Code/CommandLineApps/pbrMyFirstApp.cpp
+++ Code/CommandLineApps/pbrMyFirstApp.cpp
@@ -35,18 +35,6 @@
 #include <boost/filesystem/path.hpp>
 #endif
 
-#ifdef BUILD_OpenCV
-#include <cv.h>
-#ifdef PBREG_USE_CUDA
-#include "opencv2/core/cuda.hpp"
-#endif
-#endif
-
-#ifdef BUILD_PCL
-#include <pcl/point_types.h>
-#include <pcl/common/projection_matrix.h>
-#endif
-
 /**
  * \brief Demo file to check that #includes and library linkage is correct.
  */
@@ -75,30 +63,6 @@ int main(int argc, char** argv)
     std::cout << "Rounding to ... " << boost::math::round(0.123) << std::endl;
     boost::posix_time::ptime startTime = boost::posix_time::second_clock::local_time();
     boost::filesystem::path pathname( "/tmp/tmp.txt" );
-#endif
-
-#ifdef BUILD_OpenCV
-    cv::Matx44d matrix = cv::Matx44d::eye();
-    std::cout << "Printing 4x4 matrix ..." << std::endl << matrix << std::endl;
-
-#ifdef PBREG_USE_CUDA
-    int deviceCount = cv::cuda::getCudaEnabledDeviceCount();
-    if (deviceCount == 0)
-    {
-      pbrExceptionThrow() << "No CUDA support compiled in.";
-    }
-    if (deviceCount == -1)
-    {
-      pbrExceptionThrow() << "Cannot load CUDA driver, or CUDA is incompatible.";
-    }
-    std::cout << "Number of CUDA devices:" << deviceCount << std::endl;
-    cv::cuda::printCudaDeviceInfo(0); // just check first device, as its a proof-of-concept.
-#endif
-
-#endif // BUILD_OpenCV
-
-#ifdef BUILD_PCL
-    pcl::PointCloud<pcl::PointXYZ> cloud;
 #endif
 
     std::cout << "Calculating ... " << pbr::MyFirstAddFunction(1, 2) << std::endl;
